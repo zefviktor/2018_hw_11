fc16497da175d05e52b4a76f8ef58c08b1e6cabb
zefviktor <zefviktor@gmail.com>
2019-07-13 19:17:34 +0300

    Update 2019-07-13T16:17:30.452Z

--- build/js/masonry-foto.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-    // init Masonry after all images have loaded
-    $('.elements-gride').masonry({
-        // options
-        itemSelector: '.element-item',
-        columnWidth: 399,
-        isFitWidth: true
-    });
-});
